87ea5ad66a40f293247a391fc7e7e1a2a35b9865
mikhail <smyshmish@gmail.com>
2026-03-02 22:00:45 +0300

    доделали парсер

--- parser.py
+++ parser.py
@@ -3,20 +3,25 @@ def Parser(datafromDB):
         "M" : "Математика",
         "I" : "IT",
         "F" : "Физика",
+        "F|2" : "Физика",
+        "M|2" : "Математика",
         "A|J" : "Англиский c J",
         "A|M" : "Английский с М"
     }
+    fin = []
+    if datafromDB is None :
+        return "Сегодня занятий нет"
+    for i in datafromDB.split(","):
+        originalstr = ""
+        zzzz = i.index("-")
+        if zzzz == 1:
+            originalstr += book[i[0]]
+        elif zzzz > 1:
+            originalstr += book[i[:3]]
+        originalstr += " "
+        originalstr += i[-5:]
+        fin.append(originalstr)
+    print(fin)
+    return fin
 
-    originalstr = ""
-
-    zzzz = datafromDB.index("-")
-    if zzzz == 1:
-        originalstr += book[datafromDB[0]]
-    elif zzzz > 1:
-        originalstr += book[datafromDB[:3]]
-
-    originalstr += " "
-    originalstr += datafromDB[-5:]
-    return originalstr
-
-
+Parser("A|J-19:00,I-21:00")
